ff271e9c0a01c8bad8a8576b19f72808d8fafaea
LauraAmoroso <amorosol0@students.rowan.edu>
2018-10-04 20:10:21 -0400

    Level1
    This is the start of level one. keep in mind certain objects have tags that should be implemented throughout the project. "Player" and "Clickable" are what are made right now. This level is NOT complete.

---
+++ LauraTestLevel3/Assets/PickUp.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*Class PickUp
+ * created by Zach Reynolds
+ * Used for picking up an object and keeping it at its current size.
+ * Code should be a componant for any object that can be picked up.
+ * Make sure "Player" is the tag of the player
+ */
+
+public class PickUp : MonoBehaviour
+{
+
+    bool holding = false;
+    GameObject player;
+    Vector3 vec;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");    //This finds the PLayer object
+
+    }
+
+
+    void OnMouseDown()
+    {
+        float dis = Vector3.Distance(player.transform.position, this.transform.position);
+        dis = (dis % 10 * .03f); //this is the line of code for scaling. this line needs to be changed
+        Debug.Log(dis);
+        Vector3 newScale = transform.localScale;
+        newScale = newScale * dis;
+        transform.position = player.transform.position + (player.transform.forward * 2.0f); // to move the object in front of the player
+        transform.localScale = newScale;
+        transform.parent = player.transform;    //To move the object with the player
+
+        holding = true;
+    }
+
+     void Update()
+    {
+        if (holding == true && Input.GetMouseButtonDown(1))//right click mouse to drop item
+        {
+            transform.parent = null; //drop object
+            Debug.Log("Right clicked to drop item.");
+            holding = false; 
+        }
+    }
+
+
+}
