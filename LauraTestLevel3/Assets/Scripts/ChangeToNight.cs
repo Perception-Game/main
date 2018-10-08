499b80989197a9e5519f420c290e5d28ec6a2538
ZachCurll <zachcurll@gmail.com>
2018-10-07 20:44:24 -0400

    Added sun object, Change to night when the sun is moved
    Removed the old sky box and added a new one. Made our sun object seem more like an actual sun that can be moved and scaled. When the sun is removed from the sky the sky box will change to night.

---
+++ LauraTestLevel3/Assets/Scripts/ChangeToNight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ *Zach Curll
+ * 
+ * Changes the seen from day to night whenever the gameobject "sun" is moved.
+ * Must attach the script to the object you wish this script to perform on when moved.
+ * Must also attach the skybox you want it to change to to the "night" field.
+ * Must also attach the object to the "sun" field.
+ */
+public class ChangeToNight : MonoBehaviour {
+
+    public Material night;
+    public GameObject sun;
+    public bool moved;
+
+    public Vector3 startPos = new Vector3(50, 54.57f,0.7f);
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 sunPos = sun.transform.position;
+        if (sunPos != startPos)
+        {
+            RenderSettings.skybox = night;
+        }
+
+    }
+}
