71a5ad79871c83f143d73fa2f31eaeac6b9a8a30
reynoldsz2 <43647816+reynoldsz2@users.noreply.github.com>
2018-10-18 11:03:56 -0400

    The beginning of the castle level

---
+++ LauraTestLevel3/Assets/Castle/OpenGate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGate : MonoBehaviour {
+
+    GameObject gate;
+
+	// Use this for initialization
+	void Start () {
+        gate = GameObject.FindGameObjectWithTag("Gate");
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        gate.transform.Translate(0, 0, Time.deltaTime);
+        Debug.Log("works");
+    }
+}
