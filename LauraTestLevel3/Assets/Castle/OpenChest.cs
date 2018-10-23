11d8f66274a1e9a393e0b78e35b2b9cbafb7d960
reynoldsz2 <43647816+reynoldsz2@users.noreply.github.com>
2018-10-23 11:53:26 -0400

    updates to castle
    Made the second puzzle, and can open the chest

---
+++ LauraTestLevel3/Assets/Castle/OpenChest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenChest : MonoBehaviour {
+
+    GameObject chest;
+	// Use this for initialization
+	void Start () {
+        chest = GameObject.FindGameObjectWithTag("Chest");
+	}
+
+    // Update is called once per frame
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Key")
+        {
+            Debug.Log("yes");
+            Animator open = chest.GetComponent<Animator>();
+            open.enabled = true;
+        }
+
+    }
+}
--- LauraTestLevel3/Assets/Castle/OpenGate.cs
+++ LauraTestLevel3/Assets/Castle/OpenGate.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpenGate : MonoBehaviour {
+public class OpenGate : MonoBehaviour
+{
 
     GameObject gate;
+    GameObject door;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         gate = GameObject.FindGameObjectWithTag("Gate");
-	}
+    }
 
     public void OnTriggerEnter(Collider other)
     {
@@ -20,6 +23,18 @@ public class OpenGate : MonoBehaviour {
                 gate.transform.Translate(0, 0, -1);
 
             }
+        }
+
+        if (other.tag == "Sword")
+        {
+            door = GameObject.FindGameObjectWithTag("Door");
+            for (int x = 0; x < 30; x++)
+            {
+                door.transform.Rotate(0, 1, 0);
+            }
+            GameObject key = GameObject.FindGameObjectWithTag("Key1");
+            Renderer rend = key.GetComponent<Renderer>();
+            rend.enabled = true;
         }
     }
 }
