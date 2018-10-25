64fb874fa1181559d4725e1411af0595bf7bb200
reynoldsz2 <43647816+reynoldsz2@users.noreply.github.com>
2018-10-25 11:57:48 -0400

    Some updates

--- LauraTestLevel3/Assets/Castle/Final_Door.cs
+++ LauraTestLevel3/Assets/Castle/Final_Door.cs
@@ -4,28 +4,49 @@ using UnityEngine;
 
 public class Final_Door : MonoBehaviour {
 
-    int num = 0;
-	// Use this for initialization
-	void Start () {
-		
-	}
+    int num;
+    bool dragon1;
+    bool dragon2;
+    GameObject door;
+    // Use this for initialization
+    void Start () {
+        num = 0;
+		door = GameObject.FindGameObjectWithTag("FinalDoor");
+        dragon1 = true;
+        dragon2 = true;
+    }
 
-    // Update is called once per frame
-    public void OnTriggerEnter(Collider other)
+    void FixedUpdate()
     {
-        if (other.tag == "Dragon")
+
+        if(num == 2)
         {
+                Debug.Log("4");
             num++;
-            if(num == 2)
-            {
-                GameObject door = GameObject.FindGameObjectWithTag("FinalDoor");
+                 
                 for (int x = 0; x < 70; x++)
                 {
                     door.transform.Rotate(0, 1, 0);
                 }
-            }
+        }
+    }
 
+    // Update is called once per frame
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Dragon1" && dragon1)
+        {
+            num++;
+            dragon1 = false;
+            Debug.Log(num);
         }
 
+        if (other.tag == "Dragon2" && dragon2)
+        {
+            if(!dragon1)
+            { num = 2; }
+            dragon2 = false;
+            Debug.Log(num);
+        }
     }
 }
