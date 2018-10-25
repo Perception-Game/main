db144b297900d38c95d9b19d4fa3a753a41a027c
KyleButera <43282039+KyleButera@users.noreply.github.com>
2018-10-25 15:06:16 -0400

    Work Done on lvl

---
+++ LauraTestLevel3/Assets/Kyle's Assets/Slide_.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slide_ : MonoBehaviour
+{
+
+
+    Animator animator;
+    bool glassOpen;
+    public AudioClip GlassSound;
+
+    void Start()
+    {
+
+        glassOpen = false;
+        animator = GetComponent<Animator>();
+
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+
+            gameObject.GetComponent<AudioSource>().PlayOneShot(GlassSound);
+
+            glassOpen = true;
+            GlassControl("Open");
+
+        }
+    }
+
+
+    void OnTriggerExit(Collider col)
+    {
+
+        if (glassOpen)
+        {
+            while(col.gameObject.tag == "Player") {  }
+            
+             gameObject.GetComponent<AudioSource>().PlayOneShot(GlassSound);
+
+             glassOpen = false;
+             GlassControl("Close");
+            
+        }
+    }
+
+
+    void GlassControl(string direction)
+    {
+
+        animator.SetTrigger(direction);
+
+    }
+
+}
