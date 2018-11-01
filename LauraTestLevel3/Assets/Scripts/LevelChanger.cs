d3a7333f729affb22342bd569fae8a0bff9e7608
zcurll <33703997+zcurll@users.noreply.github.com>
2018-10-31 23:18:06 -0400

    Fixed/Level Changing
    -Added a script for level transitions
    -Worked Cave leve

---
+++ LauraTestLevel3/Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+/*
+ * Zach Curll
+ * Changes the scene to whatever string is attached to the script.
+ * The level changes when the player collides with the Object that is assigned to Portal.
+ * Object must have a collider and "isTrigger" must be selected in that objects collider.
+ * 
+ */
+
+
+public class LevelChanger : MonoBehaviour {
+
+	//Object the player will collide with to change the level.
+	public GameObject portal;
+	//Name of the level you wish to go to when the player collides with portal
+	public string nextLvl;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+
+	//When the player collides with the gameobject this script is attached to the level stated with load.
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			SceneManager.LoadScene(nextLvl);
+			//SceneManager.LoadScene("nextLvl");
+		}
+	}
+}
--- LauraTestLevel3/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ LauraTestLevel3/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -11,12 +11,12 @@ namespace UnityStandardAssets.Characters.FirstPerson
     public class FirstPersonController : MonoBehaviour
     {
         [SerializeField] private bool m_IsWalking;
-        [SerializeField] private float m_WalkSpeed;
-        [SerializeField] private float m_RunSpeed;
+        [SerializeField] public float m_WalkSpeed;
+        [SerializeField] public float m_RunSpeed;
         [SerializeField] [Range(0f, 1f)] private float m_RunstepLenghten;
-        [SerializeField] private float m_JumpSpeed;
-        [SerializeField] private float m_StickToGroundForce;
-        [SerializeField] private float m_GravityMultiplier;
+        [SerializeField] public float m_JumpSpeed;
+        [SerializeField] public float m_StickToGroundForce;
+        [SerializeField] public float m_GravityMultiplier;
         [SerializeField] private MouseLook m_MouseLook;
         [SerializeField] private bool m_UseFovKick;
         [SerializeField] private FOVKick m_FovKick = new FOVKick();
@@ -24,15 +24,15 @@ namespace UnityStandardAssets.Characters.FirstPerson
         [SerializeField] private CurveControlledBob m_HeadBob = new CurveControlledBob();
         [SerializeField] private LerpControlledBob m_JumpBob = new LerpControlledBob();
         [SerializeField] private float m_StepInterval;
-        [SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
-        [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
-        [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
+        [SerializeField] public AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
+        [SerializeField] public AudioClip m_JumpSound;           // the sound played when character leaves the ground.
+		[SerializeField] public AudioClip m_LandSound;           // the sound played when character touches back on ground.
 
         private Camera m_Camera;
         private bool m_Jump;
         private float m_YRotation;
         private Vector2 m_Input;
-        private Vector3 m_MoveDir = Vector3.zero;
+        public Vector3 m_MoveDir = Vector3.zero;
         private CharacterController m_CharacterController;
         private CollisionFlags m_CollisionFlags;
         private bool m_PreviouslyGrounded;
