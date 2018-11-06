4d18150c92a3eafd5e23ff28c2df8b66270cc3b5
reynoldsz2 <43647816+reynoldsz2@users.noreply.github.com>
2018-11-06 11:36:21 -0500

    Update with the castle level
    changed the third puzzle a bit

---
+++ LauraTestLevel3/Assets/Castle/Scripts/Place_Stairs.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Place_Stairs : MonoBehaviour {
+    public GameObject placeStairs;
+    public GameObject stairs;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Rock")
+        {
+            Instantiate(stairs, new Vector3(276.84f, 174.0f, 145.22f), placeStairs.transform.rotation);
+        }
+
+        
+    }
+}
