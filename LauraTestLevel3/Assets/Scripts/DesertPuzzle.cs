4330a9d9f803f3a88162aef7b4e34c3bbc490828
LauraAmoroso <amorosol0@students.rowan.edu>
2018-11-24 14:54:21 -0500

    Bug fixes
    Fixed mushroom errors, edited lions puzzle code to run better in Dessert Level. Edited sound code "HitGroundSound" to work because my edits to it originaly were not saved.

--- LauraTestLevel3/Assets/Scripts/DesertPuzzle.cs
+++ LauraTestLevel3/Assets/Scripts/DesertPuzzle.cs
@@ -1,7 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ * public class DesertPuzzle
+ * This class checks to see if all three colors are in their spot.
+ * If they are, the metal bars will disapear, sound will play, and the player
+ * will be able to advance to the next level.
+ * This must be attached to the metal bars game object
+ * Made by Laura Amoroso
+ * */
 public class DesertPuzzle : MonoBehaviour {
 
     GameObject redObject;
@@ -17,38 +24,51 @@ public class DesertPuzzle : MonoBehaviour {
     public Transform yellowSpot;
     bool yellowSolved = false;
 
-     void Start () {
+    public AudioSource sound; //winning sound effect
+
+    void Start () {
         redSolved = false;
         blueSolved = false;
         yellowSolved = false;
         redObject = GameObject.FindWithTag("Red");
         blueObject = GameObject.FindWithTag("Blue");
         yellowObject = GameObject.FindWithTag("Yellow");
+        sound = GetComponent<AudioSource>(); //find sound
     }
 
 	void Update () {
         //check if red is in place
         float redDistance = Vector3.Distance(redSpot.position, redObject.transform.position);//calculate distance between spot and object
-        if ((redObject.transform.position.y >= redSpot.position.y) &&(redObject.transform.position.x >= redSpot.position.x) && (redDistance < 3f))
+        float xReddistance = (redSpot.position.x - redObject.transform.position.x);
+        if ((redObject.transform.position.y >= redSpot.position.y) && (xReddistance <2) && (redDistance < 3f))
         {
             redSolved = true;
         }
         //check if blue is in place
         float blueDistance = Vector3.Distance(blueSpot.position, blueObject.transform.position);//calculate distance between spot and object
-        if ((blueObject.transform.position.y >= blueSpot.position.y) && (blueObject.transform.position.x >= blueSpot.position.x) && (blueDistance < 3f))
+        float xBluedistance = (blueSpot.position.x - blueObject.transform.position.x);
+        if ((blueObject.transform.position.y >= blueSpot.position.y) && (xBluedistance <2) && (blueDistance < 3f))
         {
            blueSolved = true;
         }
         //check if yellow is in place
         float yellowDistance = Vector3.Distance(yellowSpot.position, yellowObject.transform.position);//calculate distance between spot and object
-        if ((yellowObject.transform.position.y >= yellowSpot.position.y) && (yellowObject.transform.position.x >= yellowSpot.position.x) && (yellowDistance < 3f))
+        float xYellowdistance = (yellowSpot.position.x - yellowObject.transform.position.x);
+        if ((yellowObject.transform.position.y >= yellowSpot.position.y) && (xYellowdistance <2) && (yellowDistance < 3f))
         {
             yellowSolved = true;
         }
+
         //puzzle solved and bars disappear when all colors are in place
         if ((redSolved == true) && (blueSolved == true) && (yellowSolved == true))
         {
-            Destroy(this.gameObject); //remove bars
+            
+            sound.Play();//play sound             
+            MeshRenderer meshRenderer = GetComponent<MeshRenderer>(); //remove mesh renderer
+            meshRenderer.enabled = false;
+            MeshCollider meshCollider = GetComponent<MeshCollider>(); //remove collider
+            meshCollider.enabled = false; 
+
         }
     }
 }
