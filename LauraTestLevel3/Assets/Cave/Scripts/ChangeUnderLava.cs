4ebd7fc42d87706f86b4de6f1039c84fbb14782c
zcurll <33703997+zcurll@users.noreply.github.com>
2018-11-29 18:44:18 -0500

    Some comments
    Commented some code

--- LauraTestLevel3/Assets/Cave/Scripts/ChangeUnderLava.cs
+++ LauraTestLevel3/Assets/Cave/Scripts/ChangeUnderLava.cs
@@ -3,8 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.PostProcessing;
 
+/**
+ * This class is used to make it appear as if the player is under lava
+ * once they enter the attached collider.
+ * Once the player leaves the attached collider the screen will appear as normal.
+ * 
+ * */
 public class ChangeUnderLava : MonoBehaviour {
 
+	//PPP to set the screen as
+	//Normal should be used as the normal
+	//fx should be used as the new
 	public PostProcessingProfile normal, fx;
 
 	private PostProcessingBehaviour camImageFx;
