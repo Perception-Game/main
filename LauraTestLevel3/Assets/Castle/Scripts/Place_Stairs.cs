ffe46e24cc75aef9891fa33f3e5f3da073f7339b
reynoldsz2 <43647816+reynoldsz2@users.noreply.github.com>
2018-11-29 15:21:50 -0500

    Some Updates on the third puzzle

--- LauraTestLevel3/Assets/Castle/Scripts/Place_Stairs.cs
+++ LauraTestLevel3/Assets/Castle/Scripts/Place_Stairs.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Place_Stairs : MonoBehaviour {
     public GameObject placeStairs;
     public GameObject stairs;
+    public GameObject stairs2;
 	// Use this for initialization
 	void Start () {
 		
@@ -17,6 +18,25 @@ public class Place_Stairs : MonoBehaviour {
              Instantiate(stairs, new Vector3(276.84f, 173.7f, 145.22f), placeStairs.transform.rotation);
         }
 
-        
+        if (other.tag == "Barrel")
+        {
+
+            GameObject grandChild1;
+            GameObject grandChild2;
+            for (int x = 0; x < 11; x++)
+            {
+                grandChild1 = stairs2.gameObject.transform.GetChild(x).GetChild(0).gameObject;
+                grandChild2 = stairs2.gameObject.transform.GetChild(x).GetChild(1).gameObject;
+                Renderer rend = grandChild1.GetComponent<Renderer>();
+                rend.enabled = true;
+                Renderer rend1 = grandChild2.GetComponent<Renderer>();
+                rend1.enabled = true;
+            }
+            
+
+        }
+       
+
+
     }
 }
