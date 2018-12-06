8106cc959c400e83775d3dbe652e70169f6e576e
MacDonaldStephen <macdonals0@students.rowan.edu>
2018-12-05 21:19:52 -0500

    Still needs level transition code on object "Exit" in Mountain level, as well as a transition into the Mountain Level.

---
+++ LauraTestLevel3/Assets/Steve's Assets/create_bridge.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class create_bridge : MonoBehaviour {
+
+    //public Collider spot;
+    //public Rigidbody rigPos;
+    public MeshRenderer bridge_mesh;
+    public BoxCollider bridge_collider;
+    //private Vector3 pos;
+    bool doOnce = true;
+
+	void Start () {
+        //pos = new Vector3(rigPos.position.x, rigPos.position.y, rigPos.position.z);
+	}
+
+    void OnTriggerEnter(Collider t)
+    {
+        if (t.tag == "Small_Tree" && doOnce)
+        {
+            Debug.Log("There should be a bridge now.");
+            bridge_mesh.enabled = true;
+            bridge_collider.enabled = true;
+            doOnce = false;
+        }
+    }
+}
---
+++ LauraTestLevel3/Assets/Steve's Assets/open_wall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class open_wall : MonoBehaviour {
+
+    public GameObject p;
+    public Rigidbody wall;
+	
+    void OnTriggerEnter(Collider t)
+    {
+        if (t.name == "Player")
+            wall.useGravity = true;
+    }
+}
